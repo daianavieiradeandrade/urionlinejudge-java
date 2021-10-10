32bc8ead314a73917b2cc1381b8b7d20f159d641
Daiana Momberg de Andrade <52114937+darmiest@users.noreply.github.com>
2021-10-10 17:26:25 -0300

    Add files via upload

---
+++ src/Ex_1059.java
@@ -0,0 +1,11 @@
+public class Ex_1059 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+}
